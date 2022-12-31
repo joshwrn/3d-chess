f1257107214787ffa3cf8cb16683f51bd3959765
joshwrn <joshnwarren@gmail.com>
2022-12-30 21:51:37 -0800

    cleanup

--- src/logic/pieces/king.ts
+++ src/logic/pieces/king.ts
@@ -1,6 +1,7 @@
 import type { Board, Position } from '../board'
 import type { Move, MoveFunction, Piece, PieceFactory } from './'
-import { getFarMoves, getPiece, getMove, getBasePiece } from './'
+import { moveTypes, getFarMoves, getPiece, getMove, getBasePiece } from './'
+import type { Rook } from './rook'
 import { isRook } from './rook'
 
 export function isKing(value: King | Piece | null): value is King {
@@ -14,49 +15,40 @@ const canCastleKing = (king: King, board: Board): Move[] => {
     x: king.position.x + 3,
     y: king.position.y,
   })
+  const rook2 = getPiece(board, {
+    x: king.position.x - 4,
+    y: king.position.y,
+  })
   const spacesToRight = getFarMoves({
     board,
     piece: king,
     dir: { x: 1, y: 0 },
     propagateDetectCheck: false,
   })
-  if (isRook(rook) && !rook.hasMoved && spacesToRight.length === 2) {
-    possibleRookPositions.push({
-      newPosition: { x: 6, y: king.position.y },
-      piece: king,
-      steps: { x: 2, y: 0 },
-      capture: null,
-      type: `castling`,
-      castling: {
-        rook,
-        rookSteps: { x: -2, y: 0 },
-        rookNewPosition: { x: 5, y: king.position.y },
-      },
-    })
-  }
   const spacesToLeft = getFarMoves({
     board,
     piece: king,
     dir: { x: -1, y: 0 },
     propagateDetectCheck: false,
   })
-  const rook2 = getPiece(board, {
-    x: king.position.x - 4,
-    y: king.position.y,
+  const props = (right: boolean) => ({
+    capture: null,
+    piece: king,
+    type: moveTypes.castling,
+    newPosition: { x: right ? 6 : 2, y: king.position.y },
+    steps: { x: right ? 2 : -2, y: 0 },
+    castling: {
+      rook: right ? (rook as Rook) : (rook2 as Rook),
+      rookSteps: { x: right ? -2 : 3, y: 0 },
+      rookNewPosition: { x: right ? 5 : 3, y: king.position.y },
+    },
   })
+  if (isRook(rook) && !rook.hasMoved && spacesToRight.length === 2) {
+    possibleRookPositions.push(props(true))
+  }
+
   if (isRook(rook2) && !rook2.hasMoved && spacesToLeft.length === 3) {
-    possibleRookPositions.push({
-      newPosition: { x: 2, y: king.position.y },
-      piece: king,
-      steps: { x: -2, y: 0 },
-      capture: null,
-      type: `castling`,
-      castling: {
-        rook: rook2,
-        rookSteps: { x: 3, y: 0 },
-        rookNewPosition: { x: 3, y: king.position.y },
-      },
-    })
+    possibleRookPositions.push(props(false))
   }
   return possibleRookPositions
 }
